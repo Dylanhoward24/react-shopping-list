2999bc33787e46a9065f3294d0700f160da50acc
Michael Werneke <mike.werneke@gmail.com>
2021-08-12 16:46:01 -0500

    css

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './ShoppingList.css';
 function ShoppingList({shoppingList}) {
                     // ^ response.data from app.jsx
     //Need onButton Click 
@@ -22,9 +22,17 @@ function ShoppingList({shoppingList}) {
             <ul>
                 {shoppingList.map((item) => (
                     <li>
+                        <div class= "itemName">
                         {item.name}
+                        </div>
+                        <div class="qtyUnit">
                         {item.quantity}
                         {item.unit}
+                        </div>
+                        <div class="buyRemove">
+                        <button>Buy</button>
+                        <button>Remove</button>
+                        </div>
                     </li>
                  ))}
             </ul>
